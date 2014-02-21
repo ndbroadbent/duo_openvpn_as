57309e63ea9e6e63e7c4431545db47b8417a0796
Nathan Broadbent <nathan.f77@gmail.com>
2014-02-20 19:42:10 -0800

    sudo

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-/usr/local/openvpn_as/scripts/sacli -a openvpn -k auth.module.post_auth_script --value_file=duo_openvpn_as.py ConfigPut
-/usr/local/openvpn_as/scripts/sacli -a openvpn Reset
+sudo /usr/local/openvpn_as/scripts/sacli -a openvpn -k auth.module.post_auth_script --value_file=duo_openvpn_as.py ConfigPut
+sudo /usr/local/openvpn_as/scripts/sacli -a openvpn Reset
+
